2a218907dffbd79c06a0e548bde0782a6155f347
funcargobull <funcargobull@yahoo.com>
2024-03-02 16:58:43 +0700

    Add files via upload

---
+++ load_map.py
@@ -0,0 +1,48 @@
+from io import BytesIO
+import requests
+from PIL import Image
+
+
+def get_coords_and_address(name):
+    toponym_to_find = name
+
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+
+    address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+    # if index:
+    # toponym_index = toponym_index + ', ' + \
+    #     toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return (toponym_longitude, toponym_lattitude, address)
+
+
+def load_map(toponym_longitude, toponym_lattitude, spn, mode='map'):
+    org_point = "{0},{1}".format(toponym_longitude, toponym_lattitude)
+    map_params = {
+        "ll": ",".join([str(toponym_longitude), str(toponym_lattitude)]),
+        # "spn": ",".join([delta, delta]),
+        "z": spn,
+        "l": mode,
+        "pt": f"{org_point},pm2dgl"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+
+    return response.content
---
+++ main.py
@@ -0,0 +1,99 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+from PyQt5.QtCore import QEvent, Qt
+from PyQt5.QtGui import QPixmap, QImage
+import sys
+from load_map import *
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        uic.loadUi('design.ui', self)
+        for i in self.mode_group.buttons():
+            i.setFocusPolicy(Qt.ClickFocus)
+        self.to_search.setFocusPolicy(Qt.ClickFocus)
+
+        self.start_spn = 12
+        self.start_index = False
+        self.start_mode = "map"
+
+        self.longitude = None
+        self.lattitude = None
+        self.address = None
+
+        self.search_btn.clicked.connect(
+            lambda: self.search_button_click(self.get_user_search()))
+
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_PageUp:
+            try:
+                if self.start_spn + 1 <= 21:
+                    self.start_spn += 1
+                    self.search_button_click(self.get_user_search())
+            except KeyError:
+                pass
+        elif e.key() == Qt.Key_PageDown:
+            try:
+                if self.start_spn - 1 >= 0:
+                    self.start_spn -= 1
+                    self.search_button_click(self.get_user_search())
+            except KeyError:
+                pass
+        elif e.key() in [Qt.Key_Up, Qt.Key_Left, Qt.Key_Right, Qt.Key_Down]:
+            try:
+                if e.key() == Qt.Key_Up:
+                    if self.lattitude + 0.1 <= 180:
+                        self.lattitude += 0.1
+                elif e.key() == Qt.Key_Down:
+                    if self.lattitude - 0.1 >= 0:
+                        self.lattitude -= 0.1
+                elif e.key() == Qt.Key_Left:
+                    if self.longitude - 0.1 >= 0:
+                        self.longitude -= 0.1
+                else:
+                    if self.longitude + 0.1 <= 180:
+                        self.longitude += 0.1
+                image = load_map(self.longitude, self.lattitude,
+                                 self.start_spn, self.start_mode)
+                self.show_image(image, self.address)
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+
+    def get_user_search(self):
+        return self.to_search.text()
+
+    def search_button_click(self, toponym):
+        for i in self.mode_group.buttons():
+            if i.text() == "Схема" and i.isChecked():
+                self.start_mode = "map"
+            elif i.text() == "Спутник" and i.isChecked():
+                self.start_mode = "sat"
+            elif i.text() == "Гибрид" and i.isChecked():
+                self.start_mode = "skl"
+
+        toponym_longitude, toponym_lattitude, address = get_coords_and_address(
+            toponym)
+        self.longitude = float(toponym_longitude)
+        self.lattitude = float(toponym_lattitude)
+
+        self.address = address
+        image = load_map(self.longitude, self.lattitude,
+                         self.start_spn, self.start_mode)
+        self.show_image(image, address)
+
+    def show_image(self, image, address):
+        img = QImage()
+        img.loadFromData(image)
+        self.pixmap_label.setPixmap(QPixmap.fromImage(img))
+        self.statusBar().showMessage(address)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = App()
+    sys.exit(app.exec_())
